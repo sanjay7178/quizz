ac873f6f37e6b0680fcdf4660741a7fd86af2324
sanjay7178 <saisanjay7660@gmail.com>
2023-03-15 19:07:52 +0530

    changes have been made

---
+++ lib/shared/loading.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Loader extends StatelessWidget {
+  const Loader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: 50,
+      height: 50,
+      child: CircularProgressIndicator(),
+    );
+  }
+}
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Loader(),
+      ),
+    );
+  }
+}
